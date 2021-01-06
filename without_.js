e0773035547a22d57fed0b80ca5154ea9bf32bf8
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-06 01:23:10 +0000

    adding without_.js

---
+++ without_.js
@@ -0,0 +1,48 @@
+eqArrays = (array1,array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+assertArraysEqual = (array1,array2) => {
+  if (array1.length !== array2.length) {
+    console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
+    return;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
+        return;
+      }
+    }
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  }
+};
+
+without = (sourceArray, itemsToRemoveArray) => {
+  let newArray = [];
+  if (eqArrays(sourceArray,itemsToRemoveArray) !== false) {
+    return [];
+  } else {
+    for(i = 0; i < sourceArray.length; i++) {
+      if (itemsToRemoveArray.includes(sourceArray[i] !== true)){
+          newArray.push(sourceArray[i])
+      }   
+    }  
+  }
+}
+
+without([1, 2, 3], [1]) 
+without(["1", "2", "3"], [1, 2, "3"]) 
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+  
