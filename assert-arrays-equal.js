f6554ffca1b6c38aa746cba5b5b8d548988b4a41
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-11 23:47:35 +0000

    refactoring and adding test code for assert arrays equal

--- assert-arrays-equal.js
+++ assert-arrays-equal.js
@@ -1,18 +1,6 @@
-eqArrays = (array1,array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
 
 
-let assertArraysEqual = function(array1,array2) {
+assertArraysEqual = (array1,array2) => {
   if (array1.length !== array2.length) {
     console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
     return;
@@ -30,3 +18,5 @@ let assertArraysEqual = function(array1,array2) {
   
 assertArraysEqual([1,2,3,4],[1,2,3,4]);
 assertArraysEqual([5,8,9,4],[1,2,3,4]);
+
+module.exports = assertArraysEqual;
---
+++ test/assert_arrays_equal_test.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require('../assert-arrays-equal');
+
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
+assertArraysEqual([5,8,9,4],[1,2,3,4]);
