d85a7158c5a7b6e877a15a56b4498975ac19e9eb
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-12 00:40:43 +0000

    refactored middle_.js and added test code

--- middle_.js
+++ middle_.js
@@ -1,17 +1,4 @@
-let assertArraysEqual = (array1,array2) => {
-  if (array1.length !== array2.length) {
-    console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
-    return;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        console.log(`❌❌❌Assertion Failed: ${array1} !== ${array2}`);
-        return;
-      }
-    }
-    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  }
-};
+const assertArraysEqual = require('./assert-arrays-equal');
 
 middle = (array) => {
   let middleNumber = [];
@@ -32,3 +19,5 @@ middle = (array) => {
 
 assertArraysEqual(middle([1,2,3]),[2]);
 assertArraysEqual(middle([1,2,3,4]),[2]);
+
+module.exports = middle;
